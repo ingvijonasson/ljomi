a5a8a8cebe1cf6bb8e65f8d52b1347cbbb94e46f
ingvijonasson <ingvi101@gmail.com>
2021-08-23 12:27:38 +0200

    Setup about section
    Signed-off-by: ingvijonasson <ingvi101@gmail.com>

--- components/PageTitle.vue
+++ components/PageTitle.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="section-intro" :class="{ 'section-intro--large': isLarge }">
+  <section class="section-intro">
     <div class="container">
       <div class="section-intro__text">
         <slot></slot>
@@ -8,38 +8,12 @@
   </section>
 </template>
 
-<script>
-export default {
-  props: {
-    isLarge: {
-      type: Boolean,
-      default: false,
-    },
-  },
-}
-</script>
-
 <style scoped>
 .section-intro {
   --fluid-spacing: calc(1.5rem + 4vw);
 
   padding-top: var(--fluid-spacing);
   padding-bottom: var(--fluid-spacing);
-  background-color: var(--color-pink);
-}
-
-.section-intro__text {
-  font-size: 1.8rem;
-  font-weight: 600;
-}
-
-.section-intro--large {
-  --fluid-spacing: calc(1.5rem + 8vw);
-}
-
-.section-intro--large .section-intro__text {
-  max-width: 20em;
-  margin-left: auto;
-  margin-right: auto;
+  background-color: var(--color-base);
 }
 </style>
---
+++ components/SectionAbout.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="section-about section-large">
+    <div class="container">
+      <!-- <h2>Who</h2> -->
+      <!-- <p>
+        Freelance developer from Iceland, now living in Bern, Switzerland. A
+        musician and DJ who turned to web development in 2008, with a love for
+        design. Still in love with music, records and dance culture.
+      </p> -->
+
+      <h2>How</h2>
+      <p>
+        I treat your design with love and respect. I pay great attention to the
+        details and I make sure that everything is in its right place.
+      </p>
+      <p>
+        I’m serious about markup or the structure that lies underneath every
+        websites. Well crafted markup can do a lot of good for website and is
+        essential for responsive web design, findability in search engines and
+        accessibility.
+      </p>
+    </div>
+  </section>
+</template>

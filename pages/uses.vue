c9532336dfd5864c09fa65d14aa7c21298055c9a
ingvijonasson <ingvi101@gmail.com>
2021-09-03 17:59:02 +0200

    introduce page uses
    Signed-off-by: ingvijonasson <ingvi101@gmail.com>

---
+++ pages/uses.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <page-title>
+      <p class="text-h1">Uses</p>
+    </page-title>
+    <section class="section section-large">
+      <div class="container">
+        <h2>Editor and Terminal</h2>
+        <ul>
+          <li>Visual studio code</li>
+          <li>Tower</li>
+          <li>iTerm</li>
+        </ul>
+        <h2>Desktop apps</h2>
+        <ul>
+          <li>1Password</li>
+          <li>Alfred</li>
+          <li>Better snaptool</li>
+          <li>
+            ColorSlurp - any color picker will do this one has been working out
+            fine for me since a couple of years
+          </li>
+          <li>Dato - replace the macOs clock</li>
+          <li>Figma</li>
+          <li>Kap - for quick screen recording</li>
+          <li>Maccy</li>
+        </ul>
+
+        <h2>Saas</h2>
+        <ul>
+          <li>
+            Browserstack - for testing legacy browsers or better said ie11
+          </li>
+          <li>Harvest for time tracking and invoicing</li>
+        </ul>
+        <h2>Backup strategy</h2>
+        <h2>Desk setup</h2>
+        <h2>Audio</h2>
+        <ul>
+          <li>Ableton live 11</li>
+        </ul>
+        <h2>Miscellaneous</h2>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import PageTitle from '@/components/PageTitle.vue'
+
+export default {
+  components: {
+    PageTitle,
+  },
+}
+</script>

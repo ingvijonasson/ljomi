1bf994fa4497dc347ef0609cb0caf3502ea2ce4e
ingvijonasson <ingvi101@gmail.com>
2022-01-11 16:37:03 +0100

    Add text formatting to page uses

--- pages/uses.vue
+++ pages/uses.vue
@@ -7,28 +7,25 @@
       <div class="container">
         <h2>Editor and Terminal</h2>
         <ul>
-          <li>
-            Firefox is my main browser since 2020. Previously I was using
-            Chrome.
-          </li>
-          <li>iTerm</li>
-          <li>Visual studio code</li>
-          <li>Tower I use since 2016</li>
+          <li>Firefox - for private usage. Chrome and Edge for testing</li>
+          <li>iTerm - my prefered terminal</li>
+          <li>Visual studio code - previously using sublime text and atom</li>
+          <li>Tower - GUI for GIT</li>
         </ul>
 
         <h2>Desktop apps</h2>
         <ul>
           <li>1Password</li>
           <li>Alfred</li>
-          <li>Better snaptool</li>
+          <li>Better snaptool - for snapping windows to your screen edges</li>
           <li>
             ColorSlurp - any color picker will do this one has been working out
             fine for me since a couple of years
           </li>
-          <li>Dato - replace the macOs clock</li>
-          <li>Figma</li>
+          <li>Dato - replace the macOs clock with multiple clocks timezones</li>
+          <li>Figma - for designing</li>
           <li>Kap - for quick screen recording</li>
-          <li>Maccy</li>
+          <li>Maccy - for managing multiple cliphands</li>
         </ul>
 
         <h2>Saas</h2>
@@ -36,7 +33,7 @@
           <li>
             Browserstack - for testing legacy browsers or better said ie11
           </li>
-          <li>Harvest for time tracking and invoicing</li>
+          <li>Harvest - for time tracking and simple invoicing</li>
         </ul>
 
         <h2>Backup strategy</h2>
@@ -47,19 +44,26 @@
             ProtonDrive - for backing up certain files. I use the drive usually
             and backblaze and time machine are only for emergency situations.
           </li>
-          <li>SpiderOak for backing up everything else</li>
+          <li>SpiderOak - for backing up everything else</li>
         </ul>
 
         <!-- <h2>Hardware - Desk setup</h2> -->
 
         <h2>Audio</h2>
         <ul>
-          <li>Ableton live 11</li>
+          <li>Ableton live 11 - for editing audio files and compose</li>
+          <li>
+            RØDE NT-USB microphone and LogiLink professional Microphone Boom Arm
+            Stand - for quality audio
+          </li>
         </ul>
 
         <h2>Miscellaneous</h2>
         <ul>
-          <li>Protonmail and ProtonVPN for secure emailing and browsing</li>
+          <li>
+            Protonmail and ProtonVPN - for a basic layer of secure emailing and
+            browsing
+          </li>
         </ul>
       </div>
     </section>
@@ -75,3 +79,9 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+ul {
+  margin-top: 1rem;
+}
+</style>

9da00fb8784baea7bbdf513327c5f0c8da306ebb
ingvijonasson <ingvi101@gmail.com>
2022-01-20 16:20:38 +0100

    Update recommendation section with more visuals

--- components/SectionRecommendations.vue
+++ components/SectionRecommendations.vue
@@ -1,19 +1,22 @@
 <template>
   <section class="section section--love section--large">
     <div class="container">
-      <!-- <h2>Recommendations</h2> -->
       <h2 class="section__title">Nice words by nicer people</h2>
-      <article
-        v-for="item in recommendations"
-        :key="item.person"
-        class="recommendation"
-      >
-        <h3>{{ item.person }}</h3>
-        <h4>{{ item.title }} - {{ item.organization }}</h4>
-        <blockquote cite="https://www.linkedin.com/in/ingvijonasson/">
-          <nuxt-content :document="item" />
-        </blockquote>
-      </article>
+    </div>
+    <div class="container container--large">
+      <div class="recommendation__grid">
+        <article
+          v-for="item in recommendations"
+          :key="item.person"
+          class="recommendation"
+        >
+          <h3>{{ item.person }}</h3>
+          <h4>{{ item.title }} - {{ item.organization }}</h4>
+          <blockquote cite="https://www.linkedin.com/in/ingvijonasson/">
+            <nuxt-content :document="item" />
+          </blockquote>
+        </article>
+      </div>
     </div>
   </section>
 </template>
@@ -30,13 +33,38 @@ export default {
 </script>
 
 <style>
+.recommendation__grid {
+  display: grid;
+  grid-gap: calc(0.5rem + 1.5vw);
+  margin-top: calc(3.5rem + 1.5vw);
+}
+
 .recommendation {
-  margin-top: calc(1rem + 3vw);
+  padding: 1.2rem;
+  border-radius: 0.25rem;
+  box-shadow: 0 0 3px hsla(0, 0%, 0%, 0.2);
+  background-color: var(--color-white);
+  max-width: 700px;
+}
+
+@media only screen and (min-width: 800px) {
+  .recommendation__grid {
+    grid-template-columns: 100px 1fr 100px;
+  }
+
+  .recommendation:nth-child(odd) {
+    grid-column: 1 / 3;
+  }
+
+  .recommendation:nth-child(even) {
+    grid-column: 2 / 4;
+  }
 }
 
 .recommendation h3 {
   line-height: 1.1;
   margin-bottom: 0;
+  margin-top: 0.5rem;
 }
 
 .recommendation h4 {
